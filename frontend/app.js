3eaa99441cb42ef5f0a96927cdf338e5b88ae865
hayoung <hayoung1214@naver.com>
2021-08-13 21:38:10 +0900

    fix : 카카오 로그인 로그아웃 수정

--- frontend/app.js
+++ frontend/app.js
@@ -29,9 +29,7 @@ app.use(bodyParser.urlencoded({extended : false}));
 
 
 app.get('/', function (req, res) {
-    // if(!req.session.name)
-    //     res.redirect('/main');
-    // else   
+    
         res.redirect('/main');
 
 });
@@ -51,54 +49,8 @@ app.get('/login', function (req, res) {
 
 });
  
-app.post('/login', async function (req, res) {
-    console.log("try login")
-    try{
-        
-        data=0
-        const test_result = await axios.post('http://127.0.0.1:5000/api/v1/user/login')
-        console.log(test_result)
-        
-        
-        console.log(test_result);
-    
-        
-        
-               
-    }catch(e){console.log("[ERROR|success login] : ",e)}
-    
 
-});
 
-//logout page
-app.get('/logout',async function(req,res){
-    try{
-        
-        var test_result =  await axios.post('http://127.0.0.1:5000/api/v1/user/logout', {
-            
-            headers: { 'access_token': '1234', 'Content-Type': 'application/json' }
-            
-        });
-            
-        //res.end();  //클라이언트에게 응답을 전송한다
-        
-
-        console.log("post image to pre_test")
-        console.log(test_result)
-        console.log(test_result.status)
-        console.log(test_result.data)
-        
-        if(test_result.status == 200){
-            test_result_responsedata = test_result.data
-            console.log("ssssssssssss")
-            console.log(test_result_responsedata)
-            
-        }
-        res.json(test_result.data)       
-    }catch(e){console.log("[ERROR|success pass pre_test error] : ",e)}
-
-	res.redirect("/main");
-});
 
 
 

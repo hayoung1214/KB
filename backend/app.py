d9e2228b4b0c2b0ae6b6ab15db5a59ed3c92b4ea
hayoung1214 <hayoung1214@naver.com>
2021-08-22 09:15:37 +0000

    feat : nginx

--- backend/app.py
+++ backend/app.py
@@ -7,9 +7,9 @@ from flask_restx import Resource, Api, fields, reqparse
 from flask_cors import CORS
 import os
 from werkzeug.datastructures import FileStorage
-from detection import predict
-from torch import nn
-import torch
+#from detection import predict
+# from torch import nn
+# import torch
 
 app = Flask(__name__)
 api = Api(app, version='1.0', title='KB API',
@@ -63,34 +63,34 @@ class detect(Resource):
             response.status_code = 401
             return response
 
-class BERTClassifier(nn.Module):
-    def __init__(self,
-                 bert,
-                 hidden_size = 768,
-                 num_classes=3,   ##클래스 수 조정##
-                 dr_rate=None,
-                 params=None):
-        super(BERTClassifier, self).__init__()
-        self.bert = bert
-        self.dr_rate = dr_rate
+# class BERTClassifier(nn.Module):
+#     def __init__(self,
+#                  bert,
+#                  hidden_size = 768,
+#                  num_classes=3,   ##클래스 수 조정##
+#                  dr_rate=None,
+#                  params=None):
+#         super(BERTClassifier, self).__init__()
+#         self.bert = bert
+#         self.dr_rate = dr_rate
                  
-        self.classifier = nn.Linear(hidden_size , num_classes)
-        if dr_rate:
-            self.dropout = nn.Dropout(p=dr_rate)
+#         self.classifier = nn.Linear(hidden_size , num_classes)
+#         if dr_rate:
+#             self.dropout = nn.Dropout(p=dr_rate)
     
-    def gen_attention_mask(self, token_ids, valid_length):
-        attention_mask = torch.zeros_like(token_ids)
-        for i, v in enumerate(valid_length):
-            attention_mask[i][:v] = 1
-        return attention_mask.float()
+#     def gen_attention_mask(self, token_ids, valid_length):
+#         attention_mask = torch.zeros_like(token_ids)
+#         for i, v in enumerate(valid_length):
+#             attention_mask[i][:v] = 1
+#         return attention_mask.float()
 
-    def forward(self, token_ids, valid_length, segment_ids):
-        attention_mask = self.gen_attention_mask(token_ids, valid_length)
+#     def forward(self, token_ids, valid_length, segment_ids):
+#         attention_mask = self.gen_attention_mask(token_ids, valid_length)
         
-        _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device))
-        if self.dr_rate:
-            out = self.dropout(pooler)
-        return self.classifier(out)
+#         _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device))
+#         if self.dr_rate:
+#             out = self.dropout(pooler)
+#         return self.classifier(out)
 
 
 
